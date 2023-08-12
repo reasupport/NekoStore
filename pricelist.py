0a928e04c78499f7cfeebdcff6f35df912958b74
NekoLocal <nekodaisuke.id@gmail.com>
2023-08-13 01:30:05 +0700

    Create pricelist.py

---
+++ pricelist.py
@@ -0,0 +1,6 @@
+pricelist_data = {
+    "Jasa 1": {"harga": "Rp100.000", "label": "Layanan Pertama"},
+    "Jasa 2": {"harga": "Rp150.000", "label": "Layanan Kedua"},
+    "Jasa 3": {"harga": "Rp200.000", "label": "Layanan Ketiga"},
+    # Tambahkan jasa dan informasi lainnya sesuai kebutuhan
+}
