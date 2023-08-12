4ec2915ffb9262e770829b977eb68cc7f27e12c2
NekoLocal <nekodaisuke.id@gmail.com>
2023-08-13 01:32:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_ID = "24465982"
 API_HASH = "2b3131b7d3f6a42bd4ae1ba3b58c11c4"
-BOT_TOKEN = "ganti_dengan_token_bot_anda"
+BOT_TOKEN = "6049314071:AAGWe3ff7gSXHxK1-psj7QXeeSv_qQYFqDg"
 OWNERS = [5633222043]  # Ganti dengan daftar ID pengguna sebagai owner
